f71a4f0daee85115d943726be177bf5e9483cc07
quaresmateo <teoian.giovane@gmail.com>
2021-03-12 18:22:15 -0300

    Creates static file

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+const resources = '/resources'
+
 const server = app.listen(3000, () => {
   console.log('server is running on port', server.address().port)
 })
+
+app.use(express.static(__dirname + resources))
