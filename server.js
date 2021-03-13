b1400dcfc8d1f7bd8b98e1a77d764d2f9d08400a
quaresmateo <teoian.giovane@gmail.com>
2021-03-13 09:46:50 -0300

    Add .env file

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 const app = express()
-
 const resources = '/resources'
+
+const dbUrl = process.env.DATABASE_URL
+
+dotenv.config()
 
 const server = app.listen(3000, () => {
   console.log('server is running on port', server.address().port)
