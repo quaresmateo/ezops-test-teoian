c78e8a3257d1e7d9903097b4ee085379e704c074
quaresmateo <teoian.giovane@gmail.com>
2021-03-13 15:04:54 -0300

    Creates endpoints

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const dotenv = require('dotenv')
+const dotenv = require('dotenv').config()
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded())
 const resources = '/resources'
+const { DB_USER, DB_PASS, DB_LINK, DB_DATABASE } = process.env
 
-const dbUrl = process.env.DATABASE_URL
+/* Database connection */
+const dbUrl = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_LINK}/${DB_DATABASE}?retryWrites=true&w=majority`
+mongoose.connect(dbUrl, () => {
+  console.log('mongodb connected')
+})
 
-dotenv.config()
+/* Models */
+const Message = mongoose.model('Message', { name: String, message: String })
+
+/* Endpoints */
+app.get('/messages', (request, response) => {
+  Message.find({}, (error, messages) => {
+    response.send(messages)
+  })
+})
+
+app.post('/messages', (request, response) => {
+  const message = new Message(request.body)
+  message.save(error => {
+    if (error) {
+      response.sendStatus(500)
+    } else {
+      response.sendStatus(200)
+    }
+  })
+})
 
 const server = app.listen(3000, () => {
   console.log('server is running on port', server.address().port)
